472e3227a9bb21fe26e55e77638df32c7dd7826e
Johan Rosengren <zhirpoon@gmail.com>
2019-03-13 19:14:19 +0100

    Added solution #10

---
+++ DCP10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+/*
+    #10
+    Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+*/
+namespace DCP10
+{
+    public class DCP10
+    {
+        public static void JobScheduler(Func<string> f, int delay)
+        {
+            Thread.Sleep(delay);
+            Console.WriteLine($"delayed {f.Invoke()} by {delay} milliseconds");
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Type the amount of delay you want in milliseconds");
+            int.TryParse(Console.ReadLine(), out int delay);
+
+            Func<string> writeFunction = () => "function";
+
+            JobScheduler(writeFunction, delay);
+        }
+    }
+}
